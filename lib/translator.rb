3441b7b28f7c2e17131bb9deb1fc9604de2eb8bf
Carrie Bares <caroltbares@gmail.com>
2020-12-29 00:37:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,10 +6,10 @@ def load_library(file)
   emoticons = YAML.load_file(file)
   phash = {}
   emoticons.each do |k, v|
-    nhash[k] = {}
+    nhash = {
     k =>
     {nhash[k][:english] => v[0],
-    nhash[k][:japanese] => v[1]}
+    nhash[k][:japanese] => v[1]}}
     
     phash = phash.merge(nhash)
   end

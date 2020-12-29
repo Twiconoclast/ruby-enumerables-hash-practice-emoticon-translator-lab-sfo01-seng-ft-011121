643de8822374d97e021878f96db18d0a13df29ec
Carrie Bares <caroltbares@gmail.com>
2020-12-29 01:14:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 def get_japanese_emoticon(file, emot)
   english = load_library(file).select {|k, v| v.has_value?(emot)}
   japanese_emot = english.select {|k, v| english[k][:japanese]}
-  return japanese_emot[:japanese]
+  return japanese_emot
 end
 
 def get_english_meaning
